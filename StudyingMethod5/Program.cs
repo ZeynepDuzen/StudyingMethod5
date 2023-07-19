2c23f6dcdd9231c9d0b96db4ae3079bf286782d3
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 06:37:07 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod5
+{
+    internal class Program
+    {
+        static void FirstName(string firstName) {
+
+            Console.WriteLine(firstName + " " + "Sun");
+        }
+        static void Main(string[] args)
+        {
+            FirstName("Emy");
+            FirstName("Linda");
+            FirstName("Lucy");
+            FirstName("July");
+
+            Console.Read();
+        }
+    }
+}
